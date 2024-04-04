13d8d44e1825a98cfa92e74f52b68b6f3506448e
Emori03 <emorijan@gmail.com>
2024-04-04 17:46:50 -0600

    Eliminacion de archivos inesesarios

--- FuncionesPHP/Perfil.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    require_once ("conexion.php");
-?>
-
-<?php echo $_SESSION['Nombre']; ?>
-<?php echo $_SESSION['Rango']; ?>
-<?php echo $_SESSION['Contrasena']; ?>
--- FuncionesPHP/Retiro.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    require_once ("conexion.php");
-?>
-<?php if(isset($_POST['Confirmacion'])){?>
-    <?php echo $_POST['Cantidad']; ?>
-<?php } ?>
