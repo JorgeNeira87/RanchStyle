b0f66e68b17d08a0c8ac9b100b6cdb3d30e78e70
Emori03 <emorijan@gmail.com>
2024-04-04 20:10:38 -0600

    Actualizacion del sql

--- db_banco.sql
+++ db_banco.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-02-2023 a las 22:48:28
--- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.2.0
+-- Tiempo de generación: 05-04-2024 a las 04:10:04
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `db_banco`
+-- Base de datos: `si_banco`
 --
 
 -- --------------------------------------------------------
@@ -40,43 +40,35 @@ CREATE TABLE `registros` (
 --
 
 INSERT INTO `registros` (`id`, `usuario`, `cantidad`, `fecha`, `tipo`) VALUES
-(1, 'Emori', '100.50', '2023-02-01', 'Retiro'),
-(2, 'Emori', '50.00', '2023-02-02', 'Retiro'),
-(3, 'Emori', '50.00', '2023-02-02', 'Retiro'),
-(4, 'Emori', '100.50', '2023-02-02', 'Retiro'),
-(5, 'Emori', '50.00', '2023-02-02', 'Retiro'),
-(6, 'Emori', '50.00', '2023-02-02', 'Retiro'),
-(7, 'Emori', '50.00', '2023-02-02', 'Retiro'),
-(8, 'Emori', '50.00', '2023-02-03', 'Retiro'),
-(10, 'Emori', '100.00', '2023-02-04', 'Deposito');
+(1, 'Emori', 100.50, '2023-02-01', 'Retiro'),
+(2, 'Emori', 50.00, '2023-02-02', 'Retiro'),
+(3, 'Emori', 50.00, '2023-02-02', 'Retiro'),
+(4, 'Emori', 100.50, '2023-02-02', 'Retiro'),
+(5, 'Emori', 50.00, '2023-02-02', 'Retiro'),
+(6, 'Emori', 50.00, '2023-02-02', 'Retiro'),
+(7, 'Emori', 50.00, '2023-02-02', 'Retiro'),
+(8, 'Emori', 50.00, '2023-02-03', 'Retiro'),
+(10, 'Emori', 100.00, '2023-02-04', 'Deposito');
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `usuarios`
 --
-IF EXISTS (
-    SELECT 1 
-    FROM information_schema.tables 
-    WHERE table_schema = 'db_banco' 
-    AND table_name = 'usuarios'
-) THEN
-    DROP TABLE db_banco.usuarios;
-END IF;
-
 
 CREATE TABLE `usuarios` (
   `UsuarioID` varchar(100) NOT NULL,
   `UsuarioClavePrivada` varchar(100) NOT NULL,
-  `UsuarioClavePublica` varchar(100) NOT NULL
+  `UsuarioClavePublica` varchar(100) NOT NULL,
+  `UsuarioDatos` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`UsuarioID`, `UsuarioClavePrivada`, `UsuarioClavePublica`) VALUES
-('108848749981709851867', 'U2FsdGVkX19PxAvsmhZ+c6qLPhDVbOp6+HIVi3m73UuXqeKq8EDdRGTIBcK0VjVE', 'U2FsdGVkX19dRk5etyMMXY3f5o4TysU+LfWjNpAQ342m6g9RXpvEy+EGjSEfbhrf');
+INSERT INTO `usuarios` (`UsuarioID`, `UsuarioClavePrivada`, `UsuarioClavePublica`, `UsuarioDatos`) VALUES
+('108848749981709851867', 'U2FsdGVkX1+oT/XSHXo/uNnfFAyZLUtQkXK9aJJKTWfomcVLSJuIKIdxWEYdPr2Q', 'U2FsdGVkX18eiCEVVhg8304W+odQ/4o4e3XKLUtLymQL3EaCL65KlYzigS0rFNrP', 'U2FsdGVkX18RQfhlvmySD5DR4HHiOZr8QIHO5uMJwwoVJcRQSmCxfx34UgxlpVAsmWIDTAe4hSVuce+XShFX5FsafpI7Fd1vinyT+HzlACPBtIftNBU2e0mplr0MNyhQTXRZevdJFxT8CoopOKynbRxk1oN6Ovhs1lrUsmVXB5al6qWZ/+cl7XzmDfHH47l4djJ7Xw1NhGSSi4uwt+gR0eEjb9ux0OmhNEz24ot7R5soRP2yOUd33y9sPKf1sU5VSzSp1tY8m9Nn2wVfNdA+YFqQT4gIKC6dkd9ErdYw0BIbTHcalwDJH1lOB9h6qEqhdLCnVPQJy1q68eYtjVP2xH3vO5WMU02cybIE17PfYgakiMZEJ3XwAOffC50SE+KJi0X6qseyjVsNG5imUx3SmW5HVns2k2VEKTi8sfQtrtoNDFD8zgVMs5If7OZNTTMRNE6vIGX7HSeUWxk5N/CLZkskRTEWxFiQrD5WC5Oz24d7a5Ohv96ZcWiSYSf5G94p');
 
 --
 -- Índices para tablas volcadas
