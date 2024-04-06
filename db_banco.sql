2085b829b34ba5a09a189da1a3d230b9fa3a615a
Emori03 <emorijan@gmail.com>
2024-04-06 06:37:16 -0600

    actualizacion del sql

--- db_banco.sql
+++ db_banco.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-04-2024 a las 04:44:44
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Tiempo de generación: 06-04-2024 a las 14:36:43
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,76 +24,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `paco`
---
-
-CREATE TABLE `paco` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
-  `saldo` decimal(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `paco`
---
-
-INSERT INTO `paco` (`id`, `nombre`, `saldo`) VALUES
-(1, 'David de peña gutierres san patricio', '9965.00'),
-(2, 'Arturo gustabante Dominguez crespo don chuy', '335.00');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `registros`
---
-
-CREATE TABLE `registros` (
-  `id` int(11) NOT NULL,
-  `usuario` varchar(100) NOT NULL,
-  `cantidad` decimal(10,2) NOT NULL,
-  `fecha` date NOT NULL DEFAULT current_timestamp(),
-  `tipo` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `registros`
---
-
-INSERT INTO `registros` (`id`, `usuario`, `cantidad`, `fecha`, `tipo`) VALUES
-(1, 'Emori', '100.50', '2023-02-01', 'Retiro'),
-(2, 'Emori', '50.00', '2023-02-02', 'Retiro'),
-(3, 'Emori', '50.00', '2023-02-02', 'Retiro'),
-(4, 'Emori', '100.50', '2023-02-02', 'Retiro'),
-(5, 'Emori', '50.00', '2023-02-02', 'Retiro'),
-(6, 'Emori', '50.00', '2023-02-02', 'Retiro'),
-(7, 'Emori', '50.00', '2023-02-02', 'Retiro'),
-(8, 'Emori', '50.00', '2023-02-03', 'Retiro'),
-(10, 'Emori', '100.00', '2023-02-04', 'Deposito');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `transacciones`
 --
 
 CREATE TABLE `transacciones` (
   `id` int(11) NOT NULL,
-  `remitente_id` int(11) DEFAULT NULL,
-  `destinatario_id` int(11) DEFAULT NULL,
-  `cantidad` decimal(10,2) DEFAULT NULL,
-  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `transacciones`
---
-
-INSERT INTO `transacciones` (`id`, `remitente_id`, `destinatario_id`, `cantidad`, `fecha`) VALUES
-(3, 1, 2, '5.00', '2024-04-05 01:15:19'),
-(4, 1, 2, '10.00', '2024-04-05 01:33:16'),
-(5, 1, 2, '10.00', '2024-04-05 01:35:44'),
-(6, 1, 2, '10.00', '2024-04-05 01:36:07'),
-(7, 1, 2, '10.00', '2024-04-05 01:45:57');
+  `datos` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -106,38 +43,24 @@ CREATE TABLE `usuarios` (
   `UsuarioClavePrivada` varchar(100) NOT NULL,
   `UsuarioClavePublica` varchar(100) NOT NULL,
   `UsuarioDatos` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`UsuarioID`, `UsuarioClavePrivada`, `UsuarioClavePublica`, `UsuarioDatos`) VALUES
-('113402879819846605562', 'U2FsdGVkX19rxs7bHsC7afpQ0CvV0qbvnSEenHPOEJ590MXKrH78l/nkDOz1Uq75', 'U2FsdGVkX1/72PapNe07jyh7i6GolcR9Q5HZWHsxaFdcieXr3qNz/4Tzfwr5NOVM', 'U2FsdGVkX1//dq33bxTsmk87uH1dFmH+fovW/h7VJQZq9fhsM9PbagmBPkakLNK32G8sgLz5dh4YzXvsyA4zKa22S8guUZH4p1sVelpPYVnj9W9LOFsAJy/MLEynSou1f4UKlc2UpP29ZK1jtuuD27GD926gp3grm9xkjbIBoTbYHnkL9CRrFjqqhUSYJVokRSExgt5fylCrsbfeZszYurKxDCSqQ9bdwHEBUAramKabnqjAJPRELYfIZ5y3UMvz0DaVSIYhtusFd7oNXaPRTIAcAvt51MNE1AgTsgW8yKb6RreHrD0P2kIPHeCZzQiFuY3k4+Qai8tIST00Ogu7rA==');
+('108848749981709851867', 'U2FsdGVkX1/e5FkKjnqa73j+zfPkPU1FI1j62xbTGDUg9Wcxu3uPkjll/vucwpdT', 'U2FsdGVkX19CcJu1i1VVOGZEuxLKKacTcTQPdnAtVjaaPtGoKZy+EX50BPiYI47x', 'U2FsdGVkX1+tijoRnSu7lw9p1c54dYzxO7+osJeigG+9HJN6P4NW2ERa7JM4H3XSGw3oZO2UeELp6IADtlgFqQSVRbOj1I4060P0TpWqLa/6FRFz56nuuOBoogk5u3t+pt2CqeNYHKzk3F33rJmuof4B+j4NsQ8Uj/JLquy2N4OTPVQooXPu5/8w648rBghnsbMW+TMbMAFzwgR+NJjVyojwi2tqHLYzEH+BqPgE1mBea0uqis9aH4gqIQ/T4kQIv62UT20tbJHZa4wWGay6GgqKswTswDNfaRLu7qrAuI1oNJ5CC5gca+b2l/T0v0/g8qzZFLA36D4uehEarCYgr9WvyGIKU2IRtX8yTXDkKh+tiw7hF3F5G5PIhPgVM2DPlrKJUQvM90+CP5apX+0WLD2Jq96FJrq32XF07kUUyNz5/1an8REGQP7Jkm4T0dzijTJQpIudQSvEq7DPbQJB/9tykMq6UyfpTB/6c6dowSyoz03SIYxjKPIlwrdpVRCeuRQcfOWKCsa7igcu/UonmY7I2YvLrlJW/HFzVQ2KO5gpzWB68hG6t28dPbqZkiIDsJnT7OK/FXrY+1pqqJqIFg==');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `paco`
---
-ALTER TABLE `paco`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `registros`
---
-ALTER TABLE `registros`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indices de la tabla `transacciones`
 --
 ALTER TABLE `transacciones`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `remitente_id` (`remitente_id`),
-  ADD KEY `destinatario_id` (`destinatario_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -150,22 +73,10 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `paco`
---
-ALTER TABLE `paco`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `registros`
---
-ALTER TABLE `registros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
 -- AUTO_INCREMENT de la tabla `transacciones`
 --
 ALTER TABLE `transacciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
